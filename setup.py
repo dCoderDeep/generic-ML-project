27247bcff776d3846f469067a154af73cc3ee5f3
Deependra Rawat <rdeependra33@gmail.com>
2023-10-21 22:41:00 +0530

    setup and reqirements

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPHEN_E_DOT = "-e ." 
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [requirement.replace("\n","") for requirement in requirements]
+        
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+    return requirements
+setup(
+name="genericMLproject",
+version="0.0.1",
+author="DevDeep",
+author_email="rdeependra33@gmail.com",
+packages = find_packages(),
+install_requires=get_requirements('requirements.txt'),
+)
