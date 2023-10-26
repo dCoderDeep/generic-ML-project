2f09e4962c9c4241b5052e72594e95d3fad8c595
Deependra Rawat <rdeependra33@gmail.com>
2023-10-27 01:36:39 +0530

    data Transdormation done

--- src/exception.py
+++ src/exception.py
@@ -1,20 +1,23 @@
-import sys
-import logging
+import sys  # Import the sys module for accessing system-related information.
+import logging  # Import the logging module for logging messages.
 
-def error_message_detail(error,error_detail:sys):
-    _,_,exc_tb=error_detail.exc_info()
-    file_name=exc_tb.tb_frame.f_code.co_filename
-    error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
-     file_name,exc_tb.tb_lineno,str(error))
+# Function to generate a detailed error message.
+def error_message_detail(error, error_detail: sys):
+    # Get information about the current exception.
+    _, _, exc_tb = error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename  # Get the filename of the code.
+    
+    # Create an error message with details.
+    error_message = "Error occurred in python script name [{0}] line number [{1}] error message[{2}]".format(
+        file_name, exc_tb.tb_lineno, str(error))
 
     return error_message
 
-    
-
+# Custom exception class.
 class CustomException(Exception):
-    def __init__(self,error_message,error_detail:sys):
+    def __init__(self, error_message, error_detail: sys):
         super().__init__(error_message)
-        self.error_message=error_message_detail(error_message,error_detail=error_detail)
-    
+        self.error_message = error_message_detail(error_message, error_detail=error_detail)
+
     def __str__(self):
-        return self.error_message
+        return self.error_message  # Return the detailed error message when the exception is converted to a string.
--- src/logger.py
+++ src/logger.py
@@ -2,16 +2,21 @@ import logging
 import os
 from datetime import datetime
 
-LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
-os.makedirs(logs_path,exist_ok=True)
+# Generate a timestamp for the log file name in the format: "mm_dd_YYYY_HH_MM_SS.log"
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
-LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
+# Create a directory path for storing log files within the current working directory.
+logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
 
+# Create the 'logs' directory if it doesn't exist.
+os.makedirs(logs_path, exist_ok=True)
+
+# Define the full path to the log file.
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+
+# Configure the logging module.
 logging.basicConfig(
-    filename=LOG_FILE_PATH,
-    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO,
-
-
+    filename=LOG_FILE_PATH,  # Specify the log file path.
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",  # Define the log format.
+    level=logging.INFO,  # Set the logging level to INFO.
 )
